387890ce7146c8f383083f271aafa7fe28c9a51e
Renata Imai <renataakemii@yahoo.co.uk>
2024-10-29 08:42:18 -0300

    fixes tests and file format

--- test/test_load_dataset.py
+++ test/test_load_dataset.py
@@ -20,7 +20,7 @@ def test_load_dialog(ae_with_project, method, folder_path, load_synthetic_future
 
     elif method == "open layer":
         layer = QgsProject.instance().mapLayersByName("synthetic_future_vector")[0]
-        
+
         dialog.layer = layer
         dialog.radio_layer_matrix.setChecked(True)
 
