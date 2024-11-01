dbd97878ec5542b51dad34f9c2fc9f1f000067f0
Renata Imai <renataakemii@yahoo.co.uk>
2024-11-01 14:18:39 -0300

    update tests

--- test/test_load_project_data.py
+++ test/test_load_project_data.py
@@ -18,7 +18,7 @@ def test_no_project(ae, mocker, qtbot):
     dialog.close()
 
 
-@pytest.mark.skip("Re-write the tests - it's taking long time to run...")
+# TODO: Re-write the tests - they're really time consuming
 @pytest.mark.parametrize("button_clicked", [True, False])
 def test_project(run_assignment, mocker, qtbot, button_clicked):
     proj = run_assignment
