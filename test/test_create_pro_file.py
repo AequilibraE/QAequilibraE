60e105ec877a31ec545cf917447ba4f5951b9a3e
Renata Imai <renataakemii@yahoo.co.uk>
2025-01-06 10:35:48 -0300

    Update test_create_pro_file.py

--- test/test_create_pro_file.py
+++ test/test_create_pro_file.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
+import pytest
 
 from qaequilibrae.i18n.create_pro_file import create_file
 
 
+@pytest.mark.skip("Windows release test")
 def test_file_creation():
     create_file()
     output_path = Path("qaequilibrae/i18n/qaequilibrae.pro")
