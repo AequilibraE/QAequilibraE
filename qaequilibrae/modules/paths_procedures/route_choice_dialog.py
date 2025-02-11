681db6bc1ddae4bfde05939f43f9b85343869231
Renata Imai <renataakemii@yahoo.co.uk>
2025-02-11 13:43:01 -0300

    fix test

--- qaequilibrae/modules/paths_procedures/route_choice_dialog.py
+++ qaequilibrae/modules/paths_procedures/route_choice_dialog.py
@@ -49,8 +49,11 @@ class RouteChoiceDialog(QtWidgets.QDialog, FORM_CLASS):
         self.cob_matrices.currentTextChanged.connect(self.set_matrix)
         self.but_add_to_cost.clicked.connect(self.add_cost_function)
         self.but_clear_cost.clicked.connect(self.clear_cost_function)
+        self.but_perform_assig.clicked.connect(self.route_choice)
+        self.but_visualize.clicked.connect(self.execute_single)
 
     def __populate_project_info(self):
+        print("__populate_project_info")
 
         with read_and_close(database_connection("network")) as conn:
             res = conn.execute("""select mode_name, mode_id from modes""")
@@ -69,11 +72,13 @@ class RouteChoiceDialog(QtWidgets.QDialog, FORM_CLASS):
         self.cob_net_field.addItems(flds)
 
     def list_matrices(self):
+        print("list_matrices")
         for _, rec in self.proj_matrices.iterrows():
             if len(rec.WARNINGS) == 0:
                 self.cob_matrices.addItem(rec["name"])
 
     def set_matrix(self):
+        print("set_matrix")
         if self.cob_matrices.currentIndex() < 0:
             self.matrix = None
             return
@@ -84,11 +89,12 @@ class RouteChoiceDialog(QtWidgets.QDialog, FORM_CLASS):
             self.matrix = self.matrices.get_matrix(self.cob_matrices.currentText())
 
         self.cob_matrix_core.clear()
-        
+
         if self.matrix is not None:
             self.cob_matrix_core.addItems(self.matrix.names)
 
     def add_cost_function(self):
+        print("add_cost_function")
         params = self.ln_parameter.text()
         valid_params = self.__check_parameter_value(params)
 
@@ -104,7 +110,7 @@ class RouteChoiceDialog(QtWidgets.QDialog, FORM_CLASS):
         self.cost_function += f"{params} * {self.cob_net_field.currentText()}"
         self.txt_cost_func.setText(self.cost_function)
 
-        self.utility.extend((self.parameter, self.cob_net_field.currentText()))
+        self.utility.extend([(self.parameter, self.cob_net_field.currentText())])
 
     def clear_cost_function(self):
         self.txt_cost_func.clear()
@@ -115,6 +121,7 @@ class RouteChoiceDialog(QtWidgets.QDialog, FORM_CLASS):
         self.close()
 
     def __check_parameter_value(self, par):
+        print("__check_parameter_value")
         # parameter cannot be null
         if len(par) == 0:
             self.error = "Check parameter value."
@@ -124,11 +131,13 @@ class RouteChoiceDialog(QtWidgets.QDialog, FORM_CLASS):
         if not par.replace(".", "").replace("-", "").isdigit():
             self.error = "Wrong value in parameter."
             return False
-        
+
         self.parameter = float(par)
         return True
-    
+
     def graph_config(self):
+        print("graph_config")
+
         mode = self.cob_mode.currentText()
         mode_id = self.all_modes[mode]
         if mode_id not in self.project.network.graphs:
@@ -138,29 +147,100 @@ class RouteChoiceDialog(QtWidgets.QDialog, FORM_CLASS):
 
         field = np.zeros((1, self.graph.network.shape[0]))
         for idx, (par, col) in enumerate(self.utility):
-            x += (par * self.graph.network[col].array)
+            field += par * self.graph.network[col].array
 
-        self.graph.network["utility"] = field.reshape(self.graph.network.shape[1], 1)
+        self.graph.network = self.graph.network.assign(utility=0)
+        self.graph.network["utility"] = field.reshape(self.graph.network.shape[0], 1)
 
-        if self.chb_chosen_links.isChecked():
-            self.graph = self.project.network.graphs.pop(mode_id)
-            idx = self.link_layer.dataProvider().fieldNameIndex("link_id")
-            remove = [feat.attributes()[idx] for feat in self.link_layer.selectedFeatures()]
-            self.graph.exclude_links(remove)
-        
-        self.graph.set_graph("utility")
-        # self.graph.prepare_graph(nodes_of_interest)
+        # if self.chb_chosen_links.isChecked():
+        #     self.graph = self.project.network.graphs.pop(mode_id)
+        #     idx = self.link_layer.dataProvider().fieldNameIndex("link_id")
+        #     remove = [feat.attributes()[idx] for feat in self.link_layer.selectedFeatures()]
+        #     self.graph.exclude_links(remove)
+
         self.graph.set_blocked_centroid_flows(self.chb_check_centroids.isChecked())
 
     def matrix_config(self):
         self.matrix.computational_view([self.cob_matrix_core.currentText()])
 
     def route_choice(self):
+        print("route_choice")
+        valid_params = self.__check_route_choice_params()
+
+        if not valid_params:
+            qgis.utils.iface.messageBar().pushMessage(self.tr("Input error"), self.error, level=1, duration=5)
+            return
+
+        self.rc = RouteChoice(self.graph)
+
+        kwargs = {
+            "max_routes": self.num_routes,
+            "max_depth": self.rc_depth,
+            "penalty": float(self.penalty.text()),
+            "cutoff_prob": self.cutoff,
+            "store_results": True,
+            "beta": float(self.ln_psl.text()),
+        }
+        self.rc.set_choice_set_generation(self.cob_algo.currentText(), **kwargs)
+
+    def __check_route_choice_params(self):
+        # parameter needs to be numeric
+        if not self.max_routes.text().isdigit():
+            self.error = "Max. routes needs to be a numeric integer value"
+            return False
+
+        if not self.max_depth.text().isdigit():
+            self.error = "Max. depth needs to be a numeric integer value"
+            return False
+
+        # Check cutoff
+        if not self.ln_cutoff.text().replace(".", "").isdigit():
+            self.error = "Probability cutoff needs to be a float number"
+            return False
+
+        self.cutoff = float(self.ln_cutoff.text())
+        if self.cutoff < 0 or self.cutoff > 1:
+            self.error = "Probability cutoff assumes values between 0 and 1"
+            return False
+
+        # TODO: Check penalty
+
+        self.num_routes = int(self.max_routes.text())
+        self.rc_depth = int(self.max_depth.text())
+        return True
+
+    def execute_single(self):
+        from_node = self.__validate_node_id(self.node_from.text())
+        to_node = self.__validate_node_id(self.node_to.text())
+
+        demand = self.ln_demand.text()
+        if not demand.replace(".", "").isdigit():
+            self.error = "Wrong input value for demand"
+
+        if self.error:
+            qgis.utils.iface.messageBar().pushMessage(self.tr("Input error"), self.error, level=1, duration=5)
+            return
+
+        nodes_of_interest = np.array([from_node, to_node], dtype=np.int64)
         self.graph_config()
-        self.matrix_config()
+        self.graph.prepare_graph(nodes_of_interest)
+        self.graph.set_graph("utility")
 
-        rc = RouteChoice(self.graph)
-        rc.add_demand(self.matrix)
+        self.route_choice()
 
-        # Add a warning here
-        rc.set_choice_set_generation("bfsle", max_routes=5)
+        results = self.rc.execute_single(from_node, to_node, float(demand))
+        print(results[0])
+
+        # plot results
+
+    def __validate_node_id(self, node_id: str):
+        # Check if we have only numbers
+        if not node_id.isdigit():
+            self.error = self.tr("Wrong input value for node ID")
+
+        # Check if node_id exists
+        node_id = int(node_id)
+        if node_id not in self.__project_nodes:
+            self.error = self.tr("Node ID doesn't exist in project")
+
+        return node_id
--- test/test_route_choice.py
+++ test/test_route_choice.py
@@ -1,22 +1,54 @@
 import pytest
 from PyQt5.QtCore import Qt
 
-from qaequilibrae.modules.paths_procedures.route_choice_dialog import RouteChoice
+from qaequilibrae.modules.paths_procedures.route_choice_dialog import RouteChoiceDialog
 
 
 # TODO: test both algorithm selection
 # @pytest.mark.parametrize("save_file", [True, False])
-@pytest.mark.skip()
 def test_create_choice_set(coquimbo_project, qtbot):
-    dialog = RouteChoice(coquimbo_project)
+    dialog = RouteChoiceDialog(coquimbo_project)
 
-    dialog.node_from.setText("71645")
-    dialog.node_to.setText("79385")
-    qtbot.mouseClick(dialog.but_add_node, Qt.LeftButton)
+    # Choice set generation
+    dialog.max_routes.setText("5")
+
+    # Route choice
+    dialog.cob_net_field.setCurrentText("distance")
+    dialog.ln_parameter.setText("0.00011")
+    qtbot.mouseClick(dialog.but_add_to_cost, Qt.LeftButton)
+
+    dialog.ln_psl.setText("1.1")
+
+    # Graph configuration
+    dialog.chb_check_centroids.setChecked(False)
+
+    # path = qtbot.screenshot(dialog.tabWidget.widget(0))
+    # print(path)
+
     dialog.node_from.setText("77011")
     dialog.node_to.setText("74089")
-    qtbot.mouseClick(dialog.but_add_node, Qt.LeftButton)
+    dialog.ln_demand.setText("1.0")
 
-    dialog.cob_graph_cost.setCurrentText("distance")
-    dialog.chb_check_centroids.setChecked(False)
-    qtbot.mouseClick(dialog.but_add_graph, Qt.LeftButton)
+    # path = qtbot.screenshot(dialog.tabWidget.widget(1))
+    # print(path)
+
+    qtbot.mouseClick(dialog.but_visualize, Qt.LeftButton)
+    # dialog.execute_single()
+
+    # print(dialog.__dict__)
+
+
+# dialog.ln_parameter.setText("0,15")
+# qtbot.mouseClick(dialog.but_add_to_cost, Qt.LeftButton)
+# messagebar = dialog.iface.messageBar()
+# assert messagebar.messages[1][-1] == 'Input error:Wrong value in parameter.'
+
+# dialog.ln_parameter.setText("abc")
+# qtbot.mouseClick(dialog.but_add_to_cost, Qt.LeftButton)
+# messagebar = dialog.iface.messageBar()
+# assert messagebar.messages[1][-1] == 'Input error:Wrong value in parameter.'
+
+# dialog.ln_parameter.setText("")
+# qtbot.mouseClick(dialog.but_add_to_cost, Qt.LeftButton)
+# messagebar = dialog.iface.messageBar()
+# assert messagebar.messages[1][-1] == 'Input error:Check parameter value.'
