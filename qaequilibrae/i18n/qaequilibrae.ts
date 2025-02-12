6efce2fd6a119aeca950142a970726e57ff993c2
r-akemii <53949163+r-akemii@users.noreply.github.com>
2025-02-12 15:50:42 +0000

    Update translation file

--- qaequilibrae/i18n/qaequilibrae.ts
+++ qaequilibrae/i18n/qaequilibrae.ts
@@ -3576,7 +3576,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../modules/project_procedures/forms/ui_add_zoning.ui" line="178"/>
+        <location filename="../modules/project_procedures/forms/ui_transponet_construction.ui" line="180"/>
         <source>Layer field</source>
         <translation type="unfinished"></translation>
     </message>
